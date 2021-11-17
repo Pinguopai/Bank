ab694012bf57faf7ef9fc5a017a9f0e868f628e6
pinguopai <pinhhc0923@126.com>
2021-11-17 10:44:31 +0800

    提交了一个User 用户表

---
+++ src/main/java/weide/demo/po/User.java
@@ -0,0 +1,13 @@
+package weide.demo.po;
+
+public class User {
+    private String userNmae;
+    private int Gander;
+    private int id;
+
+    public User(String userNmae, int gander, int id) {
+        this.userNmae = userNmae;
+        Gander = gander;
+        this.id = id;
+    }
+}
